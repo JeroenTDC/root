b23d12e9e61e489b56ecb239b53313c9aa6ccc7b
Jeroen Marco Frank Stolk <JMST@nuuday.dk>
2021-04-09 11:32:50 +0200

    clean start

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
+//const node = require('@sveltejs/adapter-node');
+const node = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
