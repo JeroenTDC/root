f8cac85f136bf1d66447da793d228abbb91510a5
Jeroen Marco Frank Stolk <jmst@nuuday.dk>
2021-05-20 13:23:40 +0200

    wip

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.scss';
+</script>
+
+<nav>
+	<a href=".">Home</a>
+	<a href="about">About</a>
+	<a href="settings">Settings</a>
+</nav>
+
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 /** @type {import('@sveltejs/kit').Config} */
 
 import adapter from '@sveltejs/adapter-netlify';
+import sveltePreprocess from 'svelte-preprocess';
 
 const config = {
 	kit: {
 		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+	preprocess: sveltePreprocess()
 };
 
 export default config;
